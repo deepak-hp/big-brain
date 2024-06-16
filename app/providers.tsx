916a7532106f0ebf9062427804388bdd4dea13bf
Deepak  HP <deepakhp19@gmail.com>
2024-06-16 22:54:35 +0530

    initial convex setup

---
+++ app/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { ReactNode } from "react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
+
+interface ProvidersProps {
+  children: ReactNode; // or JSX.Element, depending on your usage
+}
+
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default Providers;
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://balanced-kodiak-10.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
