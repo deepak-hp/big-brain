e8d6359b0275bc4aee672c4e88588473ef87b520
Deepak  HP <deepakhp19@gmail.com>
2024-07-09 14:32:49 +0530

    adding vector search results to local storage and fixed style issue

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -1,48 +1,87 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchForm } from "./serch-form";
 import { api } from "@/convex/_generated/api";
 import Link from "next/link";
+import { FileIcon, NotebookPen } from "lucide-react";
+
+function SearchResult({
+  url,
+  type,
+  score,
+  text,
+}: {
+  url: string;
+  type: string;
+  score: number;
+  text: string;
+}) {
+  return (
+    <Link href={url}>
+      <li className="space-y-4 hover:bg-slate-700 bg-slate-800 rounded p-4 whitespace-pre-line">
+        <div className="flex items-center justify-between">
+          {type === "notes" ? (
+            <div className="flex gap-1 text-xl items-center">
+              <NotebookPen className="w-5 h-5" /> Note
+            </div>
+          ) : (
+            <div className="flex gap-1 text-xl items-center">
+              <FileIcon className="w-5 h-5" /> Document
+            </div>
+          )}
+          <div className="text-sm">Relevancy of {score.toFixed(2)}</div>
+        </div>
+
+        <div>{text + "..."}</div>
+      </li>
+    </Link>
+  );
+}
 
 export default function SearchPage() {
   const [results, setResults] =
     useState<typeof api.search.searchAction._returnType>(null);
+  useEffect(() => {
+    const storedResults = localStorage.getItem("searchResults");
+    if (!storedResults) return;
+    setResults(JSON.parse(storedResults));
+  }, []);
   return (
-    <main className="space-y-8 w-full">
+    <main className="space-y-8 w-full pb-24">
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold">Search</h1>
       </div>
-      <SearchForm setResults={setResults} />
+      <SearchForm
+        setResults={(searchResults) => {
+          setResults(searchResults);
+          localStorage.setItem("searchResults", JSON.stringify(searchResults));
+        }}
+      />
       <ul className="flex flex-col gap-4">
         {results?.map((result) => {
           if (result.type === "notes") {
             return (
-              <Link
-                href={`/dashboard/notes/${result.record._id}`}
-                key={result?.record?._id}
-              >
-                <li className="hover:bg-slate-700 bg-slate-800 rounded p-4 whitespace-pre-line">
-                  Type: Note {result.score}
-                  <br />
-                  <p>{result?.record?.text.substring(0, 500) + "..."}</p>
-                </li>
-              </Link>
+              <SearchResult
+                type="notes"
+                key={result?.record._id}
+                url={`/dashboard/notes/${result.record._id}`}
+                score={result.score}
+                text={result?.record?.text.substring(0, 500)}
+              />
             );
           } else {
             return (
-              <Link
-                href={`/dashboard/documents/${result.record._id}`}
-                key={result?.record?._id}
-              >
-                <li className="hover:bg-slate-700 bg-slate-800 rounded p-4 whitespace-pre-line">
-                  Type: Document {result.score}
-                  <br />
-                  <p>{result?.record?.title}</p>
-                  <p>
-                    {result?.record?.description?.substring(0, 500) + "..."}
-                  </p>
-                </li>
-              </Link>
+              <SearchResult
+                type="documents"
+                key={result?.record._id}
+                url={`/dashboard/documents/${result.record._id}`}
+                score={result.score}
+                text={
+                  result?.record?.title +
+                  ": " +
+                  result?.record?.description?.substring(0, 500)
+                }
+              />
             );
           }
         })}
